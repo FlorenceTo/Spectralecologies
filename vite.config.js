55303696d6df281ff69bcec8bee145c88c8bc581
FlorenceTo <Florenceto@live.co.uk>
2026-05-11 22:39:24 +0100

    Clarify base option in vite.config.js
    Added comment to clarify the purpose of the base option.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '',   // add this line
+  base: '',   // generates relative asset paths
   server: {
     proxy: {
       '/api/copernicus-token': {
