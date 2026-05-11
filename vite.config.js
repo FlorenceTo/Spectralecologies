7f28d679ff9236f3f0f90ed0a3803f4e627297f0
FlorenceTo <Florenceto@live.co.uk>
2026-05-11 22:38:06 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '',   // add this line
   server: {
     proxy: {
       '/api/copernicus-token': {
         target: 'https://shapps.dataspace.copernicus.eu',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/copernicus-token/, '/auth/realms/CDSE/protocol/openid-connect/token'),
-        // Ensure we handle POST requests properly
         configure: (proxy, options) => {
           proxy.on('proxyReq', (proxyReq, req, res) => {
             if (req.body) {
